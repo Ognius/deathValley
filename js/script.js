b06f9007e0d0cc7728916bba04f757b71effe895
Ognius <ognius@tut.by>
2021-07-06 14:53:53 +0300

    fix: implement 'no scroll for body when popups open' & overlay for buttons of popups

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const popupOpenImages = document.querySelectorAll('[data-popup-image]');
 const popupCloseImages = document.querySelectorAll('[data-popup-close]');
 const allPopups = document.querySelectorAll('[data-popup]');
+const body = document.querySelector("body");
 
 popupOpenImages.forEach(function (item) {
     item.addEventListener('click', function () {
@@ -10,6 +11,8 @@ popupOpenImages.forEach(function (item) {
         if (popupId === 'image-2' || popupId === 'image-6') {
             popup.querySelector('.popup__close-button').style.color = "#b83868";
         }
+
+        body.classList.add("hidden-scroll");
 
         popup.querySelector('.popup__body').addEventListener('click', function (e) {
             e.stopPropagation();
@@ -21,11 +24,13 @@ popupCloseImages.forEach(function (item) {
     item.addEventListener('click', function () {
         const popup = this.closest('[data-popup]');
         popup.classList.add('hidden');
+        body.classList.remove("hidden-scroll");
     })
 })
 
 allPopups.forEach(function (item) {
     item.addEventListener('click', function () {
         this.classList.add('hidden');
+        body.classList.remove("hidden-scroll");
     })
 })
