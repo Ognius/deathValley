58f67e3641fbf6d090d01f3ca5043e92dead8b72
Ognius <ognius@tut.by>
2021-07-08 16:07:24 +0300

    refactor: optimize code for popups (now 1 loop only)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
-const popupOpenImages = document.querySelectorAll('[data-popup-image]');
-const popupCloseImages = document.querySelectorAll('[data-popup-close]');
-const allPopups = document.querySelectorAll('[data-popup]');
+const popupOpenImages = document.querySelectorAll('.featured-works__image');
+const popupImages = document.querySelector('.popup__image');
+const popupCloseImages = document.querySelector('.popup__close-button');
+const popup = document.querySelector('.popup');
 const body = document.querySelector("body");
 
 popupOpenImages.forEach(function (item) {
     item.addEventListener('click', function () {
-        const popupId = this.dataset.popupImage;
-        const popup = document.querySelector('#' + popupId);
+        const imageSrc = this.getAttribute('src');
+        popupImages.setAttribute('src', imageSrc);
         popup.classList.remove('hidden');
-        if (popupId === 'image-2' || popupId === 'image-6') {
-            popup.querySelector('.popup__close-button').style.color = "#b83868";
-        }
-
         body.classList.add("hidden-scroll");
+
+        if (imageSrc === 'image/banner-pic2.jpg' || imageSrc === 'image/banner-pic6.jpg') {
+            popupCloseImages.style.color = "#b83868";
+        } else {
+            popupCloseImages.style.color = "#6EEEE4";
+        }
 
         popup.querySelector('.popup__body').addEventListener('click', function (e) {
             e.stopPropagation();
-        })
-    })
-})
+        });
+    });
+});
 
-popupCloseImages.forEach(function (item) {
+function closeImage(item) {
     item.addEventListener('click', function () {
-        const popup = this.closest('[data-popup]');
         popup.classList.add('hidden');
         body.classList.remove("hidden-scroll");
-    })
-})
+    });
+}
 
-allPopups.forEach(function (item) {
-    item.addEventListener('click', function () {
-        this.classList.add('hidden');
-        body.classList.remove("hidden-scroll");
-    })
-})
+closeImage(popupCloseImages);
+closeImage(popup);
