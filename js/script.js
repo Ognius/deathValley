bbb304e9fef36e76596f924478ba1f30ceaa0164
Ognius <ognius@tut.by>
2021-07-10 23:43:40 +0300

    refactor: optimize animation for popup
    45deg for popup__content

--- js/script.js
+++ js/script.js
@@ -4,31 +4,34 @@ const popupCloseImages = document.querySelector('.popup__close-button');
 const popup = document.querySelector('.popup');
 const body = document.querySelector("body");
 
-popupOpenImages.forEach(function (item) {
-    item.addEventListener('click', function () {
-        const imageSrc = this.getAttribute('src');
-        popupImages.setAttribute('src', imageSrc);
-        popup.classList.remove('hidden');
-        body.classList.add("hidden-scroll");
+if (popupOpenImages.length > 0) {
+    popupOpenImages.forEach(function (item) {
+        item.addEventListener('click', function () {
+            const imageSrc = this.getAttribute('src');
+            popupImages.setAttribute('src', imageSrc);
+            popup.classList.add('open');
+            body.classList.add('hidden-scroll');
 
-        if (imageSrc === 'image/banner-pic2.jpg' || imageSrc === 'image/banner-pic6.jpg') {
-            popupCloseImages.style.color = "#b83868";
-        } else {
-            popupCloseImages.style.color = "#6EEEE4";
-        }
+            if (imageSrc === 'image/banner-pic2.jpg' || imageSrc === 'image/banner-pic6.jpg') {
+                popupCloseImages.style.color = "#b83868";
+            } else {
+                popupCloseImages.style.color = "#6EEEE4";
+            }
 
-        popup.querySelector('.popup__body').addEventListener('click', function (e) {
-            e.stopPropagation();
+            popup.querySelector('.popup__body').addEventListener('click', function (e) {
+                e.stopPropagation();
+            });
         });
     });
-});
+}
 
 function closeImage(item) {
     item.addEventListener('click', function () {
-        popup.classList.add('hidden');
-        body.classList.remove("hidden-scroll");
+        popup.classList.remove('open');
+        body.classList.remove('hidden-scroll');
     });
 }
 
 closeImage(popupCloseImages);
 closeImage(popup);
+
